ad1d5e859ecfa501e5c40afc3e4ea1b447b6e98b
AymanMalkawi122 <malkawiayman63@gmail.com>
2023-04-17 21:47:55 +0300

    ListFinal

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,75 +1,122 @@
+
+import './Card.css'
 import Button from 'react-bootstrap/Button';
-import  Card  from 'react-bootstrap/Card';
-import { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form'
+import { useState, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+function getCardData(Card) {
+    let temp = Card;
+    temp.title = (Card.title !== undefined) ? Card.title : (Card.name !== undefined) ? Card.name : Card.item_name;
+    return temp;
+}
+
+function findIndex(arr, obj) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].data.id === obj.data.id && obj.opiration === arr[i].opiration) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function choiceStruct(obj) {
+    this.id = obj.id;
+    this.name = obj.title;
+}
 
 function CardApp(props) {
     let data = props.data;
-    const [cardData, setCard] = useState(data);
     let type = props.type;
-    let source = props.source;
+    const [cardData, setCard] = useState(getCardData(data));
+    const [starClass, setClass] = useState((type==="favorate")?"yellow":'gray');
+
     let opirationsList = props.opirationsList;
+    let choiceList = props.choiceList;
+
+    function switchHandler(element) {
+        if (element.target.checked) {
+            choiceList.current.splice(findIndex(choiceList, { data: new choiceStruct(cardData), opiration: "NOT_CHOSEN" }), 1);
+        }
+        else {
+            choiceList.current.push({ data: new choiceStruct(cardData), opiration: "NOT_CHOSEN" });
+
+        }
+        console.log(choiceList)
+    }
 
     function increaseQuantity() {
         data.quantity += 1;
         setCard(data);
 
-        let target = data.findIndex((element) => { return element === { opiration: "UPDATE", data: data } })
+        let target = findIndex(opirationsList.current, { opiration: "UPDATE", data: data });
 
         if (target === -1)
-            opirationsList.push({ opiration: "UPDATE", data: data });
+            opirationsList.current.push({ opiration: "UPDATE", data: data, type: type });
 
-        else opirationsList[target] = { opiration: "UPDATE", data: data };
+        else opirationsList.current[target] = { opiration: "UPDATE", data: data, type: type };
+        console.log(opirationsList.current)
     }
+
     function decreaseQuantity() {
         data.quantity -= 1;
         setCard(data);
 
-        let target = data.findIndex((element) => { return element === { opiration: "UPDATE", data: data } })
-
+        let target = findIndex(opirationsList.current, { opiration: "UPDATE", data: data, type: type });
+        console.log("target", target)
         if (target === -1)
-            opirationsList.push({ opiration: "UPDATE", data: data });
+            opirationsList.current.push({ opiration: "UPDATE", data: data });
 
-        else opirationsList[target] = { opiration: "UPDATE", data: data };
+        else opirationsList.current[target] = { opiration: "UPDATE", data: data, type: type };
+        console.log(opirationsList.current)
     }
 
     function toggleFavorate() {
-        // setCard(data);
 
-        let target = data.findIndex((element) => { return element === { opiration: "ADD", data: data } })
+        setClass((starClass === "gray") ? "yellow" : "gray");
+        let baseOpiration=(type==="favorate")?"DELETE":'ADD'
+        let target =findIndex(opirationsList.current, { opiration: baseOpiration, data: data, type: type })
 
-        if (target === -1)
-            opirationsList.push({ opiration: "ADD", data: data, type: type });
+        if (target === -1) {
+            opirationsList.current.push({ opiration: baseOpiration, data: data, type: type });
+        }
 
-            else opirationsList.splice(target,1);
+        else if(starClass){
+            opirationsList.current.splice(target, 1);
+        }
+        console.log(opirationsList.current)
     }
 
     return (
         <div className='Card'>
 
             <div>
-                {(type !== 'choice') ? <div onClick={toggleFavorate}>
-                    <FontAwesomeIcon icon="fa-solid fa-star" size="2xl" />
-                </div> : <Button variant="primary" onClick={increaseQuantity}>todo-Switch</Button>}
-                {(type === "favorate") ? <>
-                    <Button variant="primary" onClick={increaseQuantity}><FontAwesomeIcon icon="fa-solid fa-plus" /></Button>
-                    <Button variant="primary" onClick={decreaseQuantity}><FontAwesomeIcon icon="fa-solid fa-minus" /></Button>
-                </> : <></>
+                {(type !== 'choice') ?
+                    <div onClick={toggleFavorate}>
+                        <FontAwesomeIcon className={starClass} icon="fa-solid fa-star" size="2xl" style={{ color: "#a4a5a8", }} />
+                    </div>
+                    : <></>
+
+                }
+                {(type === "favorate") ? <div className='cardPlusMinus'>
+                    <Button variant="primary" onClick={increaseQuantity}><FontAwesomeIcon icon="fa-solid fa-plus" /> </Button>
+                    <Button variant="danger" onClick={decreaseQuantity}><FontAwesomeIcon icon="fa-solid fa-minus" /></Button>
+                </div> : <></>
                 }
             </div>
 
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
+            <Card style={{ width: '18rem' }} >
+                <Card.Img variant="top" src={`${cardData.image}`} />
 
                 <Card.Body>
-                    <Card.Title>{(type === "recipe") ? cardData.title : (type === 'ingredient' && source === 'API') ? cardData.name : cardData.item_name}</Card.Title>
+                    <Card.Title>{cardData.title}</Card.Title>
                     <Card.Text>
                         {(type === "favorate") ? cardData.quantity : <></>}
                     </Card.Text>
                 </Card.Body>
-
             </Card>
+            {(type === "choice") ? <Form.Check type="switch" id="custom-switch" className='switch' onChange={switchHandler} /> : <></>}
         </div>
     );
 }
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,38 +2,53 @@ import SearchResult from "../SearchResult/SearchResult";
 import Filter from "../Filter/Filter"
 import{ useState, useEffect } from 'react';
 export default function Search() {
+    let testData=[
+        {
+            "id": 716429,
+            "title": "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
+            "image": "https://spoonacular.com/recipeImages/716429-312x231.jpg",
+            "imageType": "jpg",
+        },
+        {
+            "id": 715538,
+            "title": "What to make for dinner tonight?? Bruschetta Style Pork & Pasta",
+            "image": "https://spoonacular.com/recipeImages/715538-312x231.jpg",
+            "imageType": "jpg",
+        }
+    ];
     const [searchRes,setsearchRes]=useState([])
     const [obj,setobj]=useState([])
 
-    async function getRecipes(){
-        setobj(obj)
-        const url= "https://my-friedge.onrender.com";
-        const response = await fetch(`${url}/complexSearch`,
-        {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
+    // async function getRecipes(){
+       
+    //     setobj(obj)
+    //     const url= "https://my-friedge.onrender.com";
+    //     const response = await fetch(`${url}/complexSearch`,
+    //     {
+    //         method: 'GET',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             'Accept': 'application/json'
 
-            },
-            query: JSON.stringify(obj)
-         } )
+    //         },
+    //         query: JSON.stringify(obj)
+    //      } )
 
 
         
-        const searchRes = await response.json();
-        setsearchRes(searchRes);
-        console.log(searchRes);
-    }
+    //     const searchRes = await response.json();
+    //     setsearchRes(searchRes);
+    //     console.log(searchRes);
+    // }
 
-    useEffect(()=>{
-        getRecipes();
-    },[])  
-
+    // useEffect(()=>{
+    //     getRecipes();
+    // },[])  
+//choice
     return(
         <>
-        <SearchResult data={searchRes} type={"choice"} source={"API"}/>
-        <p>{`${searchRes}`}</p>
+        <SearchResult data={testData} type={"recipe"} source={"API"}/>
+        <p>{`${testData}`}</p>
         <Filter searchRes={searchRes} />
         </>
     )
