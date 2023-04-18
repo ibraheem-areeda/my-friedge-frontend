786dcc6e35694382d62b3b92f4e9f50df1fa2248
ibraheem-areeda <ibraheem.areeda@gmail.com>
2023-04-18 12:36:59 +0000

    modal

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,30 +1,58 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import IngredientsModal from "../IngredientsModal/IngredientsModal";
 import Button from 'react-bootstrap/Button';
+import List from "../List/List";
 
 export default function Home() {
+    const [thelist, setthelist] = useState(false);
     const [show, setShow] = useState(false);
+console.log(thelist)
+    const handleClose = () => {
+        setShow(false);
+        setthelist(true)
+    }
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleShow = () => {
+        setShow(true);
+        setthelist(false)
+    }
+
+    async function getRandomRecipe() {
+
+
+        const baseURL = `https://my-friedge.onrender.com/randomRecipes&number=1`;
+        console.log("url base", baseURL);
+
+        const response = await fetch(baseURL,
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+
+                },
+            })
+        const searchRes = await response.json();
+        console.log(searchRes);
+    }
+
+
+    useEffect(() => {
+        getRandomRecipe()
+    }, []);
 
 
 
+    return (
 
-
-
-
-
-
-
-return(
         <div className="home">
-    <h1 >welcom to home </h1>
-    <h4 >are you hungry?</h4>
-    <Button variant="primary" type="submit" onClick={handleShow}>find a recipe</Button>
-    <IngredientsModal show={show} handleClose={handleClose}/>
+            <h1 >welcom to home </h1>
+            <h4 >are you hungry?</h4>
+            <Button variant="primary" type="submit" onClick={handleShow}>find a recipe</Button>
+            <IngredientsModal show={show} handleClose={(e) => handleClose(e)} />
+            <List />
         </div>
-    
+
     )
 }
 
--- src/components/IngredientsModal/IngredientsModal.js
+++ src/components/IngredientsModal/IngredientsModal.js
@@ -4,71 +4,72 @@ import { useState, useEffect } from "react";
 import List from '../List/List'
 
 
-export default function IngredientsModal(props){
-    const [ingredients, setIngredient] = useState([]);
+export default function IngredientsModal(props) {
+  const [ingredients, setIngredient] = useState([]);
 
-    async function getIngrediants(){
-    let url =`${process.env.REACT_APP_SERVER_URL}/allIngredient`;
+  async function getIngrediants() {
+    let url = `${process.env.REACT_APP_SERVER_URL}/allIngredient`;
     const response = await fetch(url, {
-        method: "GET",
-      });
-      const data = await response.json();
-    setIngredient(data)}
+      method: "GET",
+    });
+    const data = await response.json();
+    setIngredient(data)
+  }
 
 
 
 
 
 
-    useEffect(() => {
-        getIngrediants();
-      }, [])
+  useEffect(() => {
+    getIngrediants();
+  }, [])
 
 
 
-    return(
-    
-        
-          
+  return (
+
+
+
+    <>
+      <Modal show={props.show} onHide={props.handleClose} >
+        <Modal.Header closeButton>
+          <Modal.Title>Ingredients</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          {(ingredients.length == 0) ?
+
             <>
-                <Modal show={props.show} onHide={props.handleClose} >
-                <Modal.Header closeButton>
-                  <Modal.Title>Ingredients</Modal.Title>
-                </Modal.Header>
-        
-                <Modal.Body>
-                {(ingredients.length==0)?
-               
-                  <> 
-                <p>you don't have any ingredients in your inventory</p>
-                    
-                   <a href="/Search"><Button variant="primary" type="submit"  >
-                    look for ingredients 
-                  </Button></a>
-        
-                  <Button variant="primary" type="submit" >
-                  get random recipy
-                  </Button>
-             </>
-       
-                :<><Modal.Label>Please choose the ingredients you want for your meal</Modal.Label>
-                <List data={props.data}type={"choice"}/></>
-                }
-                </Modal.Body>
-         
-                <Modal.Footer>
-                  <Button variant="secondary" onClick={props.handleClose}>
-                    Close
-                  </Button>
-                </Modal.Footer>
-        
-        
-        
-        
-                </Modal>
-                </>)
-        
-    
-       
-    
+              <p>you don't have any ingredients in your inventory</p>
+
+              <a href="/Search"><Button variant="primary" type="submit"  >
+                look for ingredients
+              </Button></a>
+
+              <Button variant="primary" type="submit" onClick={(e)=>props.handleClose(e)}>
+                get random recipy
+              </Button>
+            </>
+
+            : <><Modal.Label>Please choose the ingredients you want for your meal</Modal.Label>
+              <List data={props.data} type={"choice"} /></>
+          }
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary" onClick={props.handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+
+
+
+
+      </Modal>
+    </>)
+
+
+
+
 }
