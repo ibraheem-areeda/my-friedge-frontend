25bf1d889d37fb328c7dbb406aa9d562b90e0105
sajedaalrababah <sajedaalrababah@icloud.com>
2023-04-18 14:03:27 +0000

    day 4

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,11 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+/* eslint-disable jsx-a11y/alt-text */
 import { useState } from "react";
 import IngredientsModal from "../IngredientsModal/IngredientsModal";
 import Button from 'react-bootstrap/Button';
+import './home.css'
+import home from'./home.jpg'
+
 
 export default function Home() {
     const [show, setShow] = useState(false);
@@ -18,13 +23,22 @@ export default function Home() {
 
 
 return(
-        <div className="home">
-    <h1 >welcom to home </h1>
-    <h4 >are you hungry?</h4>
-    <Button variant="primary" type="submit" onClick={handleShow}>find a recipe</Button>
-    <IngredientsModal show={show} handleClose={handleClose}/>
+    <>
+    <div className="home">
+
+    <div className="homeimg">
+        <img src={home}/>
+    </div>
+
+    <div className="content">
+    <h2> Don't be confused about what you will eat today </h2>
+    <h4 >Are you hungry?
+    </h4>
+
+    <Button className="button large" variant="primary" type="submit" onClick={handleShow}>find a recipe</Button>
+    <IngredientsModal show={show} handleClose={handleClose}/></div>
         </div>
-    
+        </>
     )
 }
 
