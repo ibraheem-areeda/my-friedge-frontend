9a67c59cadbff0e6993efbf47aea3443900d4a84
zekraquraan <zekraquraan7@gmail.com>
2023-04-18 14:04:08 +0000

    5:04

--- src/components/Favorates/Favorates.js
+++ src/components/Favorates/Favorates.js
@@ -1,53 +1,72 @@
-import logo from './logo.jpg'
-import Card from '../Card/Card'
-import React from 'react';
-import  Button  from 'react-bootstrap/Button';
-import {useEffect, useState} from 'react';
 
-export default function Favorates(){
-const [favRecipes,setFavRecipes ] = useState([]);
-async function getFavRecipes(){
-        let url =`${process.env.REACT_APP_SERVER_URL}/favRecipes`;
+import Card from '../Card/Card';
+import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import './Favorates.css';
 
-        let response = await fetch(url,{
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            
-            },
-})
+export default function Favorates() {
+  const [favRecipes, setFavRecipes] = useState([]);
 
-        let recivedData = await response.json();
-        setFavRecipes(recivedData)
-console.log(response)
-}
-useEffect(()=>{
+  async function getFavRecipes() {
+    let url = `${process.env.REACT_APP_SERVER_URL}/favRecipes`;
+    let response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    });
+    let receivedData = await response.json();
+    setFavRecipes(receivedData);
+    console.log(response);
+  }
+
+  useEffect(() => {
     getFavRecipes();
-},[])
-  
+  }, []);
 
+  function RecipeCards() {
+    return (
+      <>
+        {favRecipes.map((recipe) => {
+          return (
+            <>
+              <Card></Card>
+              <Button
+                variant="primary"
+                onClick={() => setFavRecipes(recipe.id)}
+              >
+                My Ingredients
+              </Button>
+              <Button
+                variant="primary"
+                onClick={() => setFavRecipes(recipe.id)}
+              >
+                My recipe
+              </Button>
+              <Button>save</Button>
+            </>
+          );
+        })}
+      </>
+    );
+  }
 
-return(<>
- <img src={logo} alt="My logo" className="logo" style={{width: '100px', height: 'auto'}} />
-<h2> this is Fav recipes Page</h2>
-{
+  function MyHeader() {
+    return (
+      <>
+        <div >
+        
+          <h1> This is Favorite Page</h1>
+        </div>
+      </>
+    );
+  }
 
-favRecipes && favRecipes.map(recipe=>{
-    return(<>
-<Card></Card>
- <Button variant="primary" onClick={()=>setFavRecipes(recipe.id)}> My Ingredants </Button>
-<Button variant="primary" onClick={()=>setFavRecipes(recipe.id)}> My recipe </Button>
-<Button>save</Button></>
-)
-    })
-    }
-
-</>
-)
-
-    
-
-
-
+  return (
+    <>
+      <MyHeader />
+      <RecipeCards />
+    </>
+  );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Footer() {
   return (
-    <MDBFooter bgColor='dark' className='text-center text-lg-start text-muted'>
+    <MDBFooter bgColor='#685858' style={{ marginTop:"120px" }} className='text-center text-lg-start text-muted'>
       <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
-        <div className='me-5 d-none d-lg-block'>
-          <span>Get connected with us on social networks:</span>
+        <div className='text-uppercase fw-bold mb-4'style={{ color: '#75b91d'  }}>
+          <span>CONTACT US:</span>
         </div>
 
         <div>
           <a href='https://www.facebook.com' className='me-4 text-reset'>
-            <MDBIcon fab icon="facebook-f" />
+            <MDBIcon  icon="facebook-f" style={{ color: 'blue' }} />
           </a>
           <a href='https://twitter.com' className='me-4 text-reset'>
-            <MDBIcon fab icon="twitter" />
+            <MDBIcon  icon="twitter" style={{ color: 'blue' }}/>
           </a>
           <a href='https://google.com' className='me-4 text-reset'>
-            <MDBIcon fab icon="google" />
+            <MDBIcon  icon="google" style={{ color: 'red' }} />
           </a>
           <a href='https://instgram.com' className='me-4 text-reset'>
-            <MDBIcon fab icon="instagram" />
+            <MDBIcon  icon="instagram" style={{ color: 'pink' }}/>
           </a>
           <a href='https://github.com' className='me-4 text-reset'>
-            <MDBIcon fab icon="github" />
+            <MDBIcon  icon="github" style={{ color: 'black' }}/>
           </a>
         </div>
       </section>
@@ -32,13 +33,13 @@ export default function Footer() {
         <MDBContainer className='text-center text-md-start mt-5'>
           <MDBRow className='mt-3'>
             <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'style={{ color: '#75b91d'  }}>
                 <MDBIcon icon="gem" className="me-3" />
-              About 
+              About US :
               </h6>
               <p>
-                Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
-                consectetur adipisicing elit.
+              Our mission is to give our customers peace of mind when it comes to food selection.
+        
               </p>
             </MDBCol>
 
@@ -46,14 +47,14 @@ export default function Footer() {
             
 
             <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Contact us :</h6>
+              <h6 className='text-uppercase fw-bold mb-4'style={{ color: '#75b91d'  }}>Contact Information:</h6>
               <p>
                 <MDBIcon icon="home" className="me-2" />
                  location: Jordan , Amman 
               </p>
               <p>
                 <MDBIcon icon="envelope" className="me-3" />
-                 email: info@my_freadge.com
+                 email: info@my_fridge.com
               </p>
               <p>
                 <MDBIcon icon="phone" className="me-3" />
@@ -65,9 +66,9 @@ export default function Footer() {
       </section>
 
       <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
-        © 2023 Copyright:
-        <a className='text-reset fw-bold' href='j'>
-           MY-FREDGE.com
+        © 2023 Copyright
+        <a className='text-reset fw-bold' href='j' style={{ padding: '350px'  }}>
+                  MY-FRIDGE.com
         </a>
       </div>
     </MDBFooter>
