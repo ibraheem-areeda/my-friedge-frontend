7f9b6e3746bd2d594ad58942a1d46612274af6c9
AymanMalkawi122 <malkawiayman63@gmail.com>
2023-04-18 22:38:24 +0300

    style+linkedComponrnts

--- src/components/IngredientsModal/IngredientsModal.js
+++ src/components/IngredientsModal/IngredientsModal.js
@@ -8,84 +8,39 @@ import './modle.css'
 
 export default function IngredientsModal(props) {
   const choiceList = useRef([]);
-  const ingredients=props.ingredients;
+  const ingredients = props.ingredients;
 
- 
+
 
   function getRandom(params) {
     props.handleClose();
   }
 
-
-
-
-  return (
-
-
+  return ((ingredients === "loading") ? <Spinner animation="border" /> :
 
     <>
       <Modal show={props.show} onHide={props.handleClose} >
         <Modal.Header closeButton>
           <Modal.Title>Ingredients</Modal.Title>
         </Modal.Header>
+
         <Modal.Body>
-          {(ingredients==="loading")?<Spinner animation="border" />:(ingredients.length === 0) ?
+          {(ingredients.length === 0) ?
 
             <>
+              <p>You don't have any ingredients in your inventory</p>
+              <div className="btn">
+                <a href="/Search"><Button variant="primary" type="submit"  >
+                  look for ingredients
+                </Button></a>
 
-                <Modal show={props.show} onHide={props.handleClose} >
-                <Modal.Header closeButton>
-                  <Modal.Title>Ingredients</Modal.Title>
-                </Modal.Header>
-        
-                <Modal.Body>
-                {(ingredients.length==0)?
-               
-                  <> 
-                <p>You don't have any ingredients in your inventory</p>
-                    <div className="btn">
-                   <a href="/Search"><Button variant="primary" type="submit"  >
-                    look for ingredients 
-                  </Button></a>
-                     
-                  <Button variant="primary" type="submit" >
+                <Button variant="primary" type="submit" >
                   get random recipy
-                  </Button></div>
-             </>
-       
-                :<><Modal.Label>Please choose the ingredients you want for your meal</Modal.Label>
-                <List data={props.data}type={"choice"}/></>
-                }
-                </Modal.Body>
-         
-                <Modal.Footer>
-                  <Button  variant="secondary" onClick={props.handleClose}>
-                    Close
-                  </Button>
-                </Modal.Footer>
-        
-        
-        
-        
-                </Modal>
-                </>)
-      
-}
-     /*  <p>you don't have any ingredients in your inventory</p>
-
-              <a href="/Search"><Button variant="primary" type="submit"  >
-                look for ingredients
-              </Button></a>
-
-              <Button variant="primary" type="submit" onClick={getRandom}>
-                get random recipy
-              </Button>
+                </Button></div>
             </>
 
-            : <>
-            <Modal.Title>Please choose the ingredients you want for your meal</Modal.Title>
-              <List data={ingredients} type={"choice"} choiceList={choiceList}/>
-              </>
+            : <><Modal.Title>Please choose the ingredients you want for your meal</Modal.Title>
+              <List data={ingredients} type={"choice"} /></>
           }
         </Modal.Body>
 
@@ -94,10 +49,9 @@ export default function IngredientsModal(props) {
             Close
           </Button>
         </Modal.Footer>
-
-
-
-
       </Modal>
-    </>)
-*/
+    </>
+  )
+
+}
+
